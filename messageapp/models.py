309455c8ebcf9f48f44468f35caab90ea20b3f4a
anvarbek009 <anvarasqarov2009@gmail.com>
2024-06-15 20:50:56 +0500

    Anvarbek

--- messageapp/models.py
+++ messageapp/models.py
@@ -5,8 +5,20 @@ class Group(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'groups'
+
 class Contact(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'contacts'
 
 
 
@@ -20,3 +32,10 @@ class Message(models.Model):
     group = models.ForeignKey(Group, on_delete=models.DO_NOTHING)
     user = models.ForeignKey('users.CustomUser', on_delete=models.DO_NOTHING)
 
+
+    def __str__(self):
+        return self.text
+    
+    class Meta:
+        db_table = 'messages'
+        
